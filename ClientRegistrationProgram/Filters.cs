eb199821527f74e26041095a044f1b632620e129
lustre2 <heyitsmetasha@gmail.com>
2026-01-14 01:00:39 +0000

    Start on filtering

---
+++ ClientRegistrationProgram/Filters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClientRegistrationProgram
+{
+    public class Filters
+    {
+        public string Email { get; set; }
+        public string Name { get; set; }
+
+        public Filters() { }
+    }
+}
--- ClientRegistrationProgram/Form1.cs
+++ ClientRegistrationProgram/Form1.cs
@@ -6,16 +6,19 @@ namespace ClientRegistrationProgram
     public partial class CustomerApp : Form
     {
 
+        Filters _filters;
         ClientDataManager _clientManager;
 
         public CustomerApp()
         {
             InitializeComponent();
+            ClientDatabase.Init();
             _clientManager = new ClientDataManager(CustomerDataGridView);
             _clientManager.SetupDataGrid();
             _clientManager.ChangeAdminState(AdminCheckBox.Checked);
             _clientManager.ChangeSignedInState(SignedInCheckBocx.Checked);
-            ClientDatabase.Init();
+
+            _filters = new Filters();
         }
 
         private void AddButton_Click(object sender, EventArgs e)
@@ -35,7 +38,7 @@ namespace ClientRegistrationProgram
 
             bool success = _clientManager.TryAddClient(client);
 
-            if (success) 
+            if (success)
             {
                 ClearInputs();
             }
@@ -74,7 +77,8 @@ namespace ClientRegistrationProgram
         /// <summary>
         /// Clears all client input data sections
         /// </summary>
-        private void ClearInputs() {
+        private void ClearInputs()
+        {
             NameInputTxt.Clear();
             AddressInputTxt.Clear();
             NumberInputTxt.Clear();
@@ -84,6 +88,18 @@ namespace ClientRegistrationProgram
             GamesAddCheckBox.Checked = false;
             OfficeToolsAddCheckBox.Checked = false;
             AccessoriesAddCheckBox.Checked = false;
+        }
+
+        private void NameInputTxt_TextChanged(object sender, EventArgs e)
+        {
+            _filters.Name = NameInputTxt.Text;
+            _clientManager.FilterDataGrid(_filters);
+        }
+
+        private void EmailInputTxt_TextChanged(object sender, EventArgs e)
+        {
+            _filters.Email = EmailInputTxt.Text;
+            _clientManager.FilterDataGrid(_filters);
         }
     }
 }
